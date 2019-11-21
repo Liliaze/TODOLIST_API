93bc27f5a3533fc81a30f95404d32dfbd2f8009e
Liliaze <diana.boudy@gmail.com>
2019-11-21 14:59:31 +0100

    Polish router.php

--- Router/Router.php
+++ Router/Router.php
@@ -5,32 +5,58 @@
  * Date: 12/11/2019
  * Time: 11:58
  */
-
 require_once './Controller/AccountController.php';
 require_once "./Controller/TaskListController.php";
 require_once './Exception/RouterException.php';
-
 class Router
 {
+    public function run() {
 
-    private $_url;
-    private $_argcUrl;
-    private $_data;
-    private $_method;
-    private $_header;
+        if (isset($_SERVER['REQUEST_METHOD'])) {
 
-    public function __construct() {
-        $this->_url = '';
-        $this->_argcUrl = 0;
-        $this->_method = '';
+            $method = $_SERVER['REQUEST_METHOD'];
+
+            if (isset($_GET['url'])) {
+
+                //get route
+                $url = explode('/',$_GET['url']);
+                $nbParameterInUrl = count($url);
+
+                //check if the routes contains /api/ in first parameter
+                if ($url[0] != "api") {
+                    throw new RouterException('path request need contains [api] in first parameter');
+                }
+
+                //save POST parameters in $_data
+                $data = json_decode(file_get_contents('php://input'), true);
+
+                //save header parameters in $_header
+                $header = apache_request_headers();
+
+                //call controllers by request method
+                switch ($method) {
+                    case 'GET' :
+                        return $this->methodGetRoutes($url, $nbParameterInUrl, $header, $data);
+                        break;
+                    case 'POST' :
+                        return $this->methodPostRoutes($url, $nbParameterInUrl, $header, $data);
+                        break;
+                    case 'DELETE' :
+                        return $this->methodDeleteRoutes($url, $nbParameterInUrl, $header, $data);
+                        break;
+                    default:
+                        throw new RouterException('No Request Method Matches');
+                }
+            }
+        }
+        throw new RouterException('No routes matches or missing argument');
     }
 
-    private function getRoutes($urlParameterArray, $argumentCount, $headerData, $parameterData) {
-
+    private function methodGetRoutes($urlParameterArray, $argumentCount, $headerData, $parameterData) {
         //GET' /api/authToken
         if ($argumentCount == 2 && $urlParameterArray[1] == "authToken")
             return \AccountController::getInstance()->login($headerData);
-                //'GET' /api/taskList
+        //'GET' /api/taskList
         else if ($argumentCount == 2 && $urlParameterArray[1] == "taskList")
             return \TaskListController::getInstance()->getUserTaskLists($headerData);
         //'GET' /api/taskList/{idList}
@@ -39,11 +65,10 @@ class Router
         //'GET' /api/taskList/{taskListId}/tasks
         else if ($argumentCount == 4 &&  $urlParameterArray[1] == "taskList" && ctype_digit($urlParameterArray[2]) && $urlParameterArray[3] == "tasks")
             return TaskListController::getInstance()->getTasks($headerData, $urlParameterArray[2]);
-        else
-            throw new RouterException('No routes matches');
+        throw new RouterException('No routes matches or missing argument');
     }
 
-    private function postRoutes($urlParameterArray, $argumentCount, $headerData, $parameterData)
+    private function methodPostRoutes($urlParameterArray, $argumentCount, $headerData, $parameterData)
     {
         //'POST' /api/user
         if ($argumentCount == 2 && $urlParameterArray[1] == "user")
@@ -60,10 +85,10 @@ class Router
         //'POST' /api/task/{taskId}
         else if ($argumentCount == 3 &&  $urlParameterArray[1] == "task" && ctype_digit($urlParameterArray[2]))
             return TaskListController::getInstance()->updateTask($headerData, $urlParameterArray[2], $parameterData);
-        throw new RouterException('No routes matches');
+        throw new RouterException('No routes matches or missing argument');
     }
 
-    private function deleteRoutes($urlParameterArray, $argumentCount, $headerData, $parameterData)
+    private function methodDeleteRoutes($urlParameterArray, $argumentCount, $headerData, $parameterData)
     {
         //'DELETE' /api/taskList/{taskListId}
         if ($argumentCount == 3 && $urlParameterArray[1] == "taskList" && ctype_digit($urlParameterArray[2]))
@@ -71,46 +96,6 @@ class Router
         //'DELETE' /api/task/{taskId}
         else if ($argumentCount == 3 && $urlParameterArray[1] == "task" && ctype_digit($urlParameterArray[2]))
             return TaskListController::getInstance()->deleteTask($headerData, $urlParameterArray[2]);
-        else
-            throw new RouterException('No routes matches');
-    }
-
-    public function run() {
-        if (isset($_SERVER['REQUEST_METHOD'])) {
-            $this->_method = $_SERVER['REQUEST_METHOD'];
-            if (isset($_GET['url'])) {
-
-                //get route
-                $this->_url = explode('/',$_GET['url']);
-                $this->_argcUrl = count($this->_url);
-
-                //check if the routes contains /api/ in first parameter
-                if ($this->_url[0] != "api") {
-                    throw new RouterException('path request need contains [api] in first parameter');
-                }
-
-                //save POST parameters in $_data
-                $this->_data = json_decode(file_get_contents('php://input'), true);
-
-                //save header parameters in $_header
-                $this->_header = apache_request_headers();
-
-                //call controllers by request method
-                switch ($this->_method) {
-                    case 'GET' :
-                        return $this->getRoutes($this->_url, $this->_argcUrl, $this->_header, $this->_data);
-                        break;
-                    case 'POST' :
-                        return $this->postRoutes($this->_url, $this->_argcUrl, $this->_header, $this->_data);
-                        break;
-                    case 'DELETE' :
-                        return $this->deleteRoutes($this->_url, $this->_argcUrl, $this->_header, $this->_data);
-                        break;
-                    default:
-                        throw new RouterException('No Request Method Matches');
-                }
-            }
-        }
         throw new RouterException('No routes matches or missing argument');
     }
 }
